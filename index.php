22f36b251a3d3e8e206a74ae710c3d5c12aa350b
JnckCode <54052722+jnckcode@users.noreply.github.com>
2020-05-08 01:34:03 +0700

    Add file script v2 beta
    Beta (v2)

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="icon" href="logo.png" type="image/png" size="16x16">
+<meta name="theme-color" content="#10ac84">
+    <meta name="msapplication-navbutton-color" content="#10ac84">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#10ac84">
+    <meta property="og:title" content="Api-AutoResponder">
+    <meta property="og:url" content="https://api-autoresponder.herokuapp.com/">
+    <meta property="og:description" content="Rest-Api {AutoResponder For Whatsapp} By JnckCode, to build automation reply via AutoResponder apps android.">
+
+	<title>Api-AutoResponder</title>
+	<meta name="description" content="Rest-Api {AutoResponder For Whatsapp} By JnckCode, to build automation reply via AutoResponder apps android.">
+	<meta name="keyword" content="AutoResponder Whatsapp,Rest-Api,autoreply Whatsapp,bot Whatsapp">
+  <style type="text/css">
+ *{
+	background-color: #222f3e;
+	color: #fff;
+	font-family: "roboto",arial;
+	margin: 0 auto;
+	padding: 30 auto;
+  }
+
+  #header{
+    width: 100%;
+    height: 7vh;
+    background-color: #10ac84;
+    font-size: 2.5vw;
+  }
+ 
+  #header:hover{
+  	background-color: #1dd1a1;
+  }
+
+ a{
+  	background-color: transparent;
+  	text-decoration: none;
+  }
+
+  input{
+  	border-radius: 5px;
+  	border-color: #10ac84;
+  }
+
+  #content{
+  	width: 70%;
+  	height: auto;
+  	text-align:left;
+  	margin-top: 8vh;
+    margin-bottom: 6vh;
+  }
+
+  #artikel{
+  	font-size: 5vw;
+  }
+
+  #logo{
+    width:50%;
+  }
+
+  #step{
+    margin-top: 5vh;
+  }
+
+  #penutup{
+    margin-top: 4vh;
+  }
+
+pre{
+	width: auto;
+	background-color: #bdc3c7;
+	color: #2c3e50;
+	border-left: 6px solid;
+	border-color: #27ae60;
+	padding: 7px;
+	border-radius: 4px;
+	/*overflow-x: scroll;*/
+}
+
+#label{
+	margin-top: 2%;
+}
+
+#footer{
+    width: 100%;
+    height: 10vh;
+    background-color: #10ac84;
+    text-align: center;
+    padding-top: 10px;
+  }
+
+ #kaki{
+  background-color: transparent;
+ }
+
+ #credit{
+  background-color: transparent;
+  font-size: 2vw;
+ }
+
+  @media only screen and (max-width:480px){
+  	#header{
+    font-size: 35px;
+    vertical-align: middle;
+    display: block;
+    padding-top: 20px;
+  }
+  #content{
+  	margin-top: 4vh;
+  }
+  #artikel{
+  	font-size: 28px;
+  }
+
+
+  } /*tutup responsive*/
+  </style>
+</head>
+<body>
+<nav id="header" align="center">
+	<a href="#">{Bot-Reply whatsapp}</a>
+</nav>
+<div id="logo" align="center">
+<img src="logo.png" width="220" height="220">
+</div>
+<div id="content">
+<span id="artikel">Hello Slur !</span><br>
+<p>ini adalah project JnckCode yang menurut saya tidak berkesan, tapi paling suka diOprek ketika gabut.. hehe<br>
+pernah gak sih kalian menemukan ide disaat sedang menikmati berbuka puasa siang hari ? sama tentunya, saya juga belum pernah wkwkwk.</p>
+<p>Oke..jadi gini slur, ini project saya buat setelah gak sengaja banget liat aplikasi autoresponder for whatsapp nih, singkat cerita saya ngewe dulu agar peredaran darah lancar supaya tidak stuck saat coding. setelah berkenthu hebat saya memutuskan untuk mengolah pattern response yang sudah dibagikan oleh sang developer.</p>
+<p>Dengan gabungan beberapa api-rest untuk downloader maka jadilah project ini, project yang gak jelas buat apa intinya sekedar iseng saat menghabiskan waktu #DiRumahAja, okehh.... mari saya jelaskan terkait api-rest ini : </p>
+<br>
+<div id="label">
+<label for="fb">Rest-APi for Facebook Download :</label>
+<br>
+<pre>https://api-autoresponder.herokuapp.com/fb.php</pre>
+</div>
+<div id="label">
+<label for="ig">Rest-APi for Instagram Download :</label>
+<br>
+<pre>https://api-autoresponder.herokuapp.com/ig.php</pre>
+</div>
+<div id="label">
+<label for="ig">Rest-APi for Youtube Download :</label>
+<br>
+<pre>https://api-autoresponder.herokuapp.com/yt.php</pre>
+</div>
+<div id="step">
+<span id="artikel">Usage :</span><br><br>
+<p>1. Download AutoResponder For Whatsapp, lalu install !.</p>
+<p>2. Tambah Rule kamu dengan set regex (regular expresion) jangan lupa slur, pake opsi expert pattern matching.</p>
+<p>3. Lalu pilih opsi Connect your web server dan isi URL dengan rest-api yang sudah saya berikan diatas.</p>
+<p>4. Simpan Rules, Done !.</p>
+</div>
+
+<div id="penutup">
+  <p>Terimakasih buat kalian yang sudah menghargai project ini, misalnya nih kalian bingung.... boleh kok nanya ke kontak dibawah. hehe asal jangan curhat aja dahh owkowkwowko.</p>
+</div>
+
+</div>
+
+<div id="footer">
+  <span id="kaki">KONTAK : <a href="mailto:jnckfamz@gmailcom">jnckfamz@gmail.com</a> | <a href="https://api.whatsapp.com/send?phone=082287839940">WA = 0822-8783-9940</a> | <a href="https://www.facebook.com/ex.core404">Facebook = Rezha Andriyan</a></span>
+  <p id="credit">Powered By JnckCode - {SGBTeam} - {KangFreshtea} - {Divisi-JM}</p>
+</div>
+</body>
+</html>
