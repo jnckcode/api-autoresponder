1635c99aa5aa36f03a874c2c010f59173da23e39
JnckCode <54052722+jnckcode@users.noreply.github.com>
2020-07-02 23:27:43 +0700

    Update some sh*t

--- index.php
+++ index.php
@@ -120,6 +120,24 @@ pre{
   </style>
 </head>
 <body>
+	<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2605067402927345',
+      xfbml      : true,
+      version    : 'v7.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 <nav id="header" align="center">
 	<a href="#">{Bot-Reply whatsapp}</a>
 </nav>
